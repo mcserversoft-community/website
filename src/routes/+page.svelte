a35ad063394032386e1afdad5a1483d013bfa65d
Poulette <eliotb1357@gmail.com>
2023-01-03 22:40:25 +0100

    Dynamically import the projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
-<script>
+<script lang="ts" >
+	import ProjectCard from '$lib/components/projectCard.svelte';
 	import Hero from '$lib/layouts/hero.svelte';
-	import Projects from '$lib/layouts/projects.svelte';
+
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
 
 <Hero />
-<Projects />
+
+<section>
+	<div class="container px-5 py-6 mx-auto">
+		<div class="flex flex-wrap -m-4">
+			{#each data.projects as { name, language, url } }
+			<ProjectCard {name} description={language}  {url} />
+			{/each}
+		</div>
+	</div>
+</section>
