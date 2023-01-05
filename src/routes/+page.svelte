f4ca4fc9870869eeac1e2b34e172407cec841d5d
Poulette <eliotb1357@gmail.com>
2023-01-05 21:06:38 +0100

    Moved `+page.ts` functions to page script (#6)
    Made a few changes to mimic the behavior of the separate script (async / await)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
 	import ProjectCard from '$lib/components/projectCard.svelte';
 	import Hero from '$lib/layouts/hero.svelte';
 
-	import type { PageData } from './$types';
-	export let data: PageData;
+	export let data: any
+	async function getProjects() {
+		const res = await self.fetch('https://api.github.com/orgs/mcserversoft-community/repos');
+		data = await res.json();
+		return data;
+	}
+	
 </script>
 
 <Hero />
@@ -11,9 +16,11 @@
 <section>
 	<div class="container px-5 py-6 mx-auto">
 		<div class="flex flex-wrap -m-4">
-			{#each data.projects as { name, language, html_url } }
-			<ProjectCard {name} description={language}  url={html_url} />
-			{/each}
+			{#await getProjects() then projects}
+				{#each projects as { name, html_url, language }}
+				<ProjectCard {name} description={language}  url={html_url} />
+				{/each}
+			{/await}
 		</div>
 	</div>
 </section>
